ccb2b962adb1f64701893c1cb7811fe1ba43144b
mehrab poorzakaria <99506317+mmehrab-pz@users.noreply.github.com>
2023-06-25 04:51:35 +0330

    Update master.js

--- js/master.js
+++ js/master.js
@@ -1,6 +1,8 @@
 function open_menu(){
     document.getElementById('menu').style.right = '0'
+    document.getElementById('menu').style.right = 'visible'
 }
 function close_menu(){
     document.getElementById('menu').style.right = '-30%'
+    document.getElementById('menu').style.right = 'hidden'
 }
